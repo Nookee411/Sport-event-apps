8fde99f9facec249bf3c2880ebfc39bb5f4eb42a
Votintsev Daniil <daniil43rus@list.ru>
2021-06-23 07:11:08 +0300

    Configured manifest for 3 activities

--- app/src/main/java/ru/example/sportevents/ui/main/MainActivity.java
+++ app/src/main/java/ru/example/sportevents/ui/main/MainActivity.java
@@ -1,15 +1,16 @@
 package ru.example.sportevents.ui.main;
 
-import android.app.Activity;
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+import android.os.Bundle;
 
+import ru.example.sportevents.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
