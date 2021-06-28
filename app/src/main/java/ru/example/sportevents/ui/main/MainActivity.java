27c805941f5b0e8dc5d4015ed6c124a0556f97b8
Votintsev Daniil <daniil43rus@list.ru>
2021-06-28 12:04:51 +0300

    Import from database

--- app/src/main/java/ru/example/sportevents/ui/main/MainActivity.java
+++ app/src/main/java/ru/example/sportevents/ui/main/MainActivity.java
@@ -1,11 +1,24 @@
 package ru.example.sportevents.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.customview.widget.ViewDragHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
 
 import ru.example.sportevents.R;
 
@@ -16,14 +29,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        Log.d("MAIN", "onCreate: "+ myRef);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                Log.d("MAIN", "onDataChange: "+ snapshot.getValue());
+                HashMap<String,HashMap<String,String>> value = (HashMap<String,HashMap<String,String>>)snapshot.getValue();
+                Log.d("MAIN", "Value is: " + value);
+
+                value.values().forEach(ele->{
+                    Log.d("MAIN", "onDataChange: "+ ele.get("name") + " "+ ele.get("location"));
+                    eventsAdapter.events.add(new Event(ele.get("name"), ele.get("location"),ele.get("location")));
+                    eventsAdapter.notifyDataSetChanged();
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         setContentView(R.layout.activity_main);
         eventsAdapter = new EventListAdapter();
         eventList = findViewById(R.id.EventList);
         eventList.setLayoutManager(new LinearLayoutManager(this));
         eventList.setAdapter(eventsAdapter);
-//        chatMessageList.scrollToPosition(messageListAdapter.messageList.size()-1);
-
         eventsAdapter.events.add(new Event("Первое событие", "Первое описание события","Киров"));
         eventsAdapter.notifyDataSetChanged();
+        eventsAdapter.events.add(new Event("Второе событие", "Второе описание события","Киров"));
+        eventsAdapter.notifyDataSetChanged();
     }
+
+
 }
